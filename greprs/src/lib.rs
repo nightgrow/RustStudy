de2a9632071ca5e282b7801154e51dff4e5ea8bc
nightgrow <40855664+nightgrow@users.noreply.github.com>
2020-06-24 20:08:42 +0900

    greprs
    add error

---
+++ greprs/src/lib.rs
@@ -0,0 +1,55 @@
+use std::fs::File;
+use std::io::prelude::*;
+use std::error::Error;
+
+pub struct Config {
+    query: String,
+    filename: String,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+
+        let query = args[1].clone();
+        let filename = args[2].clone();
+
+        Ok(Config {query, filename})
+    }
+}
+
+pub fn run(config: Config) -> Result<(), Box<Error>> {
+    let mut f = File::open(config.filename)?;
+
+    let mut contents = String::new();
+    f.read_to_string(&mut contents)?;
+
+    println!("With test:\n{}", contents);
+
+    Ok(())
+}
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    vec![]
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+        assert_eq!(
+            vec!["safe, fast, productive."],
+            search(query, contents)
+        );
+    }
+}
---
+++ greprs/target/debug/deps/greprs-875eb59d3339edc9.d
@@ -0,0 +1,7 @@
+C:\Users\윤이온\Desktop\Ion Yun\Study\Git\RustStudy\greprs\target\debug\deps\greprs-875eb59d3339edc9.rmeta: src\lib.rs
+
+C:\Users\윤이온\Desktop\Ion Yun\Study\Git\RustStudy\greprs\target\debug\deps\libgreprs-875eb59d3339edc9.rlib: src\lib.rs
+
+C:\Users\윤이온\Desktop\Ion Yun\Study\Git\RustStudy\greprs\target\debug\deps\greprs-875eb59d3339edc9.d: src\lib.rs
+
+src\lib.rs:
